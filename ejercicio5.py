ac8d48d0df8f95beb51b5c58ebb7bc10327075d0
mvillalobos712 <mvillalobos712@gmail.com>
2017-06-01 14:50:24 -0400

    Create ejercicio5.py

---
+++ ejercicio5.py
@@ -0,0 +1,10 @@
+#descripcion: crear un programa que que devuelva una lista que contenga elementos comunes entre la lista sin duplicados
+#entrada: a=[1,1,2,3,5,8,13,21,34,55,89,1,2,3,4,5,6,7,8,9,10,11,12,13]
+#salida: resucios de la lista sin duplicados 
+#autor: mvillalobos712
+#version: 2.0
+#plataforma: python 2.7
+
+a = [1,1,2,3,5,8,13,21,34,55,89,1,2,3,4,5,6,7,8,9,10,11,12,13]
+list(set(a))
+[1,2,3,4,5,6,7,8,9,10,11,12,13,21,34,55,89]
